51dda2fa3ffd650290a9b237a01d800447f81cfb
shimizu-k-digi <shimizu-k@sunrisedigital.jp>
2017-03-21 16:36:16 +0900

    画像削除API作成、リサイズ画像含めてファイル名が入ったものすべて削除できるように。

--- web/delete-api.php
+++ web/delete-api.php
@@ -1,25 +1,31 @@
 <?php
-require_once dirname(__FILE__) . '/ImageProxy/Image.php';
-require_once dirname(__FILE__) . '/ImageProxy/Http.php';
 
 if(isset($_POST["path"]))
 {
-  $file_path = '/home/source/sites/stand-alone/img.fuzoku-db.jp/web/ip/f/' . $_POST["path"];
+  include dirname($_SERVER['SCRIPT_FILENAME']).'/config.php';
+  $file_path =  "./" . $settings["img_dir"] . "/" . $_POST["path"];
+  
   if(file_exists($file_path))
-  {
-    try {
-      unlink($file_path);
+  {   
+    $path_info = pathinfo($file_path);
+    try 
+    {
+      //リサイズされた画像を含めファイル名が含まれているものを全て削除
+      foreach (scandir($path_info["dirname"]) as $file)
+      {
+        if(strpos($file, $path_info["basename"]) !== false)
+        {
+          unlink($path_info["dirname"] . "/" . $file);
+          echo $path_info["dirname"] . "/" . $file;
+        }
+      }      
       $response = "DONE";
     } catch (Exception $ex) {
-      $response = "FAILED";
+      $response = "Failed ". $ex->getMessage();  
     }
   }else {
     $response = "File Not Exists " . $file_path;
   }
   
   echo $response;
-}
-else
-{
-  echo "bb";
 }
